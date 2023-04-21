c3f92ff5cbb37e4aebdcc5513293170a45f1eb3e
Dakota <95160810+urdakota@users.noreply.github.com>
2023-04-21 14:03:49 -0400

    s

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-async function getName(authToken) {    
-  const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+%27 + authToken);    
-  const data = await response.json();    
-  return data.name
-};
-async function addTokens() {   
-  const myToken = localStorage.token.split(%27JWT %27)[1];    
-  const response = await fetch("https://api.blooket.com/api/users/add-rewards", {        
-    method: "PUT",        
-    headers: {            
-      "referer": "https://www.blooket.com/",            
-      "content-type": "application/json",            
-      "authorization": localStorage.token        
-    },        
-    body: JSON.stringify({            
-      name: await getName(myToken),            
-      addedTokens: 1000,            
-      addedXp: 300        
-    })    
-  });    
-  if (response.status == 200) {        
-    alert(`1000 added to your account!`);    
-  } else {        
-    alert("An error occured.");    
-  };
-};
-addTokens();
+const delay = prompt("Wordlist Link","https://www.vocabulary.com/lists/8378603");
+const delay = prompt("Delay per answer (MS)",5000);
+
