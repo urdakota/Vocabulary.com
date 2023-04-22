f43b68ca5b7d610f559d33fcf9815f5212aee98a
Dakota <95160810+urdakota@users.noreply.github.com>
2023-04-22 16:39:55 -0400

    updated information

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ This is a project i made for fun because im too lazy to do my vocab
 ### What it do
 - Gets the question
 - Checks a list of previous questions for that question
-- if its not on the list, it randomly clicks till it gets the answer
-- if its on the list, it clicks the answer
+- if its not on the list, it randomly guesses the answer
+- if its on the list, it puts the answer
 
 ## WARNING
 > *This can get your account **RESTRICTED** or **BANNED** if you set the delay too small*
-> Id recommend around 5 seconds in delay
+> Id recommend around 2.5 seconds in delay
 
 ## HOW TO RUN
 Make a new bookmark with this as the link:
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function sleep (time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-var count = 0;
 var isrunning = false;
 function main() {
     
@@ -13,100 +12,102 @@ function main() {
     var isrunning = true;
     var id = 0;
     var screens = document.querySelector("#challenge > div > div.questionPane");
-    screens.querySelectorAll("div").forEach(element => {
-        if (element.getAttribute("data-slot-id") > id) {
-            id = element.getAttribute("data-slot-id");
-        }
-    });
-    var current = screens.children[id];
-    var question = current.querySelector("div > section.left > div.question");
-
-    var questionContent = question.querySelector("div.questionContent");
-    var next = document.querySelector("#challenge > div > div:nth-child(2) > button");
-    
-    var questiontype = "";
-    if (question.querySelector("div.spelltheword")) {
-        questiontype = "spelltheword";
-    } else if(question.querySelector("div.choices")){
-        questiontype = "choices"
-    }
-
-    var word = ""
-    var realquestion = ""
-    switch (questiontype) {
-        case "spelltheword":
-            word = questionContent.querySelector("div.sentence.complete").getElementsByTagName("strong")[0].innerText;
-            question.querySelector("div.spelltheword > div.field.left > input").value = word;
-            setTimeout(() => {
-                question.querySelector("div.spelltheword > div.field.right > button.spellit.ss-write.left").click();
-                isrunning = false;
-            }, delay);
-            break;
-        case "choices":
-
-            var choices = question.querySelector("div.choices");
-            var instructions = question.querySelector("div.instructions");
-            if (questionContent.children.length == 0) {
-                realquestion = instructions.innerText;
-                word = instructions.getElementsByTagName("STRONG")[0].innerText
-            } else {
-                realquestion = questionContent.children[0].innerText
-                word = questionContent.children[0].getElementsByTagName("STRONG")[0].innerText
+    if (screens) {
+        screens.querySelectorAll("div").forEach(element => {
+            if (element.getAttribute("data-slot-id") > id) {
+                id = element.getAttribute("data-slot-id");
             }
+        });
+        var current = screens.children[id];
+        var question = current.querySelector("div > section.left > div.question");
 
-            if (questions[realquestion]) {
-                choices.querySelectorAll("a").forEach(element => {
-                    if (questions[realquestion] == element.innerText) {
-                        element.click();
-                        setTimeout(() => {
-                            if (element.getAttribute("class") === "correct") {
-                                isrunning = false;
-                            }
-                            clicked = false;
-                        }, 1000);
-                    }
-                });
-            } else {
-                var lifeLines = question.querySelector("div.lifeLines");
-                var lifeLineContent = question.querySelector("div.lifeLineContent");
-                if (lifeLines && lifeLines.getAttribute("style") != "display:none;") {
-                    lifeLines.querySelector("span:nth-child(2) > a:nth-child(1)").click();
-                }
+        var questionContent = question.querySelector("div.questionContent");
+        var next = document.querySelector("#challenge > div > div:nth-child(2) > button");
+        
+        var questiontype = "";
+        if (question.querySelector("div.spelltheword")) {
+            questiontype = "spelltheword";
+        } else if (question.querySelector("div.choices")) {
+            questiontype = "choices"
+        }
+
+        var word = ""
+        var realquestion = ""
+        switch (questiontype) {
+            case "spelltheword":
+                word = questionContent.querySelector("div.sentence.complete").getElementsByTagName("strong")[0].innerText;
+                question.querySelector("div.spelltheword > div.field.left > input").value = word;
                 setTimeout(() => {
+                    question.querySelector("div.spelltheword > div.field.right > button.spellit.ss-write.left").click();
+                    isrunning = false;
+                }, delay);
+                break;
+            case "choices":
+
+                var choices = question.querySelector("div.choices");
+                var instructions = question.querySelector("div.instructions");
+                if (questionContent.children.length == 0) {
+                    realquestion = instructions.innerText;
+                    word = instructions.getElementsByTagName("STRONG")[0].innerText
+                } else {
+                    realquestion = questionContent.children[0].innerText
+                    word = questionContent.children[0].getElementsByTagName("STRONG")[0].innerText
+                }
+
+                if (questions[realquestion]) {
                     choices.querySelectorAll("a").forEach(element => {
-                        function clickbtn(element) {
-                            if (choices.getElementsByClassName("correct").length === 0) {
-                                if (element.getAttribute("class") != "incorrect") {
-                                    if (clicked) {
-                                        setTimeout(() => {
-                                            clickbtn(element)
-                                        }, 1000);
-                                    } else {
-                                        clicked = true;
-                                        element.click();
-                                        setTimeout(() => {
-                                            if (element.getAttribute("class") === "correct") {
-                                                questions[realquestion] = element.innerText;
-                                                console.log(`${realquestion} ${element.innerText}`)
-                                                isrunning = false;
-                                            }
-                                            clicked = false;
-                                        }, 1000);
+                        if (questions[realquestion] == element.innerText) {
+                            element.click();
+                            setTimeout(() => {
+                                if (element.getAttribute("class") === "correct") {
+                                    isrunning = false;
+                                }
+                                clicked = false;
+                            }, 1000);
+                        }
+                    });
+                } else {
+                    var lifeLines = question.querySelector("div.lifeLines");
+                    var lifeLineContent = question.querySelector("div.lifeLineContent");
+                    if (lifeLines && lifeLines.getAttribute("style") != "display:none;") {
+                        lifeLines.querySelector("span:nth-child(2) > a:nth-child(1)").click();
+                    }
+                    setTimeout(() => {
+                        choices.querySelectorAll("a").forEach(element => {
+                            function clickbtn(element) {
+                                if (choices.getElementsByClassName("correct").length === 0) {
+                                    if (element.getAttribute("class") != "incorrect") {
+                                        if (clicked) {
+                                            setTimeout(() => {
+                                                clickbtn(element)
+                                            }, 1000);
+                                        } else {
+                                            clicked = true;
+                                            element.click();
+                                            setTimeout(() => {
+                                                if (element.getAttribute("class") === "correct") {
+                                                    questions[realquestion] = element.innerText;
+                                                    console.log(`${realquestion} ${element.innerText}`)
+                                                    isrunning = false;
+                                                }
+                                                clicked = false;
+                                            }, 1000);
+                                        }
                                     }
                                 }
                             }
-                        }
-                        setTimeout(clickbtn(element), 2000);
-                    });
-                }, 1000);
-            }
-            break;
+                            setTimeout(clickbtn(element), 2000);
+                        });
+                    }, 1000);
+                }
+                break;
+        }
+    } else {
+        isrunning = false;
     }
-        
 
     function next_btn() {
         if (next.getAttribute("class") == "next active" && !isrunning) {
-            console.log("next")
             next.click();
             setTimeout(() => {
                 console.log("restarted")
@@ -119,7 +120,6 @@ function main() {
         }, 1000);
     }
     next_btn();
-    count ++
 }
 
 main();
