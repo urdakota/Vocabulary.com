11dd21ec74610ff1a9b30da850441d92dba2e9c2
Dakota <95160810+urdakota@users.noreply.github.com>
2023-04-22 17:10:07 -0400

    image testing

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ function main() {
         var next = document.querySelector("#challenge > div > div:nth-child(2) > button");
         
         var questiontype = current.getAttribute("data-slide-type");
-        var word = ""
         switch (questiontype) {
             case "spelling":
-                word = questionContent.querySelector("div.sentence.complete").getElementsByTagName("strong")[0].innerText;
+                var word = questionContent.querySelector("div.sentence.complete").getElementsByTagName("strong")[0].innerText;
                 question.querySelector("div.spelltheword > div.field.left > input").value = word;
                 setTimeout(() => {
                     question.querySelector("div.spelltheword > div.field.right > button.spellit.ss-write.left").click();
@@ -29,60 +28,103 @@ function main() {
             case "choice":
                 var realquestion = ""
                 var choices = question.querySelector("div.choices");
-                var instructions = question.querySelector("div.instructions");
-                if (questionContent.children.length == 0) {
-                    realquestion = instructions.innerText;
-                    word = instructions.getElementsByTagName("STRONG")[0].innerText
-                } else {
-                    realquestion = questionContent.children[0].innerText
-                    word = questionContent.children[0].getElementsByTagName("STRONG")[0].innerText
-                }
+                if (question.querySelector("div.instructions")) {
+                    var instructions = question.querySelector("div.instructions");
+                    if (questionContent.children.length == 0) {
+                        realquestion = instructions.innerText;
+                    } else {
+                        realquestion = questionContent.children[0].innerText
+                    }
 
-                if (questions[realquestion]) {
+                    if (questions[realquestion]) {
+                        choices.querySelectorAll("a").forEach(element => {
+                            if (questions[realquestion] == element.innerText) {
+                                element.click();
+                                setTimeout(() => {
+                                    if (element.getAttribute("class") == "correct") {
+                                        isrunning = false;
+                                    }
+                                    clicked = false;
+                                }, 1000);
+                            }
+                        });
+                    } else {
+                        var lifeLines = question.querySelector("div.lifeLines");
+                        var lifeLineContent = question.querySelector("div.lifeLineContent");
+                        if (lifeLines && lifeLines.getAttribute("style") != "display:none;") {
+                            lifeLines.querySelector("span:nth-child(2) > a:nth-child(1)").click();
+                        }
+                        setTimeout(() => {
+                            choices.querySelectorAll("a").forEach(element => {
+                                function clickbtn(element) {
+                                    if (choices.getElementsByClassName("correct").length == 0) {
+                                        if (element.getAttribute("class") != "incorrect") {
+                                            if (clicked) {
+                                                setTimeout(() => {
+                                                    clickbtn(element)
+                                                }, 1000);
+                                            } else {
+                                                clicked = true;
+                                                element.click();
+                                                setTimeout(() => {
+                                                    if (element.getAttribute("class") == "correct") {
+                                                        questions[realquestion] = element.innerText;
+                                                        console.log(`${element.innerText}: ${realquestion}`)
+                                                        isrunning = false;
+                                                    }
+                                                    clicked = false;
+                                                }, 1000);
+                                            }
+                                        }
+                                    }
+                                }
+                                setTimeout(clickbtn(element), 2000);
+                            });
+                        }, 1000);
+                    }
+                } else {
+                    var word = question.querySelector("div.word > div.wrapper").innerText;
+
                     choices.querySelectorAll("a").forEach(element => {
-                        if (questions[realquestion] == element.innerText) {
+                        if (questions[element.getAttribute("style")]) {
                             element.click();
                             setTimeout(() => {
-                                if (element.getAttribute("class") === "correct") {
+                                if (element.getAttribute("class") == "correct") {
                                     isrunning = false;
                                 }
                                 clicked = false;
                             }, 1000);
                         }
-                    });
-                } else {
-                    var lifeLines = question.querySelector("div.lifeLines");
-                    var lifeLineContent = question.querySelector("div.lifeLineContent");
-                    if (lifeLines && lifeLines.getAttribute("style") != "display:none;") {
-                        lifeLines.querySelector("span:nth-child(2) > a:nth-child(1)").click();
-                    }
-                    setTimeout(() => {
-                        choices.querySelectorAll("a").forEach(element => {
-                            function clickbtn(element) {
-                                if (choices.getElementsByClassName("correct").length === 0) {
-                                    if (element.getAttribute("class") != "incorrect") {
-                                        if (clicked) {
-                                            setTimeout(() => {
-                                                clickbtn(element)
-                                            }, 1000);
-                                        } else {
-                                            clicked = true;
-                                            element.click();
-                                            setTimeout(() => {
-                                                if (element.getAttribute("class") === "correct") {
-                                                    questions[realquestion] = element.innerText;
-                                                    console.log(`${element.innerText}: ${realquestion}`)
-                                                    isrunning = false;
-                                                }
-                                                clicked = false;
-                                            }, 1000);
+                    })
+                    if(isrunning){
+                        setTimeout(() => {
+                            choices.querySelectorAll("a").forEach(element => {
+                                function clickbtn(element) {
+                                    if (choices.getElementsByClassName("correct").length === 0) {
+                                        if (element.getAttribute("class") != "incorrect") {
+                                            if (clicked) {
+                                                setTimeout(() => {
+                                                    clickbtn(element)
+                                                }, 1000);
+                                            } else {
+                                                clicked = true;
+                                                element.click();
+                                                setTimeout(() => {
+                                                    if (element.getAttribute("class") == "correct") {
+                                                        questions[element.getAttribute("style")] = word;
+                                                        console.log(`${word}: ${element.getAttribute("style")}`)
+                                                        isrunning = false;
+                                                    }
+                                                    clicked = false;
+                                                }, 1000);
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            setTimeout(clickbtn(element), 2000);
-                        });
-                    }, 1000);
+                                setTimeout(clickbtn(element), 2000);
+                            });
+                        }, 1000);
+                    }
                 }
                 break;
         }
